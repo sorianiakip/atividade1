68f6ca79cc711281c47b510b1771532e447c1488
ana <ana.soriani.nunes@escola.pr.gov.br>
2026-08-24 16:32:38 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-# Exercício 02 - Operadores e cálculos em Python
-nota1 = float(input("Digite a primeira nota: ").replace(",", "."))
-nota2 = float(input("Digite a segunda nota: ").replace(",", "."))
-minutos_estudo = int(input("Digite o total de minutos estudados: "))
-questoes_corretas = int(input("Digite a quantidade de questões corretas: "))
-total_questoes = int(input("Digite o total de questões: "))
+# Exercício 03 - Condições com if e else 
+nome = input("Digite seu nome: ").strip()
+idade = int(input("Digite sua idade: "))
+idade_minima = 14
 
-soma_notas = nota1 + nota2
-media = round(soma_notas / 2,2)
-diferenca = abs(nota1 - nota2)
-horas_estudo = minutos_estudo // 60
-minutos_restantes = minutos_estudo %  60
-aproveitamento = round((questoes_corretas / total_questoes) * 100, 1)
-pontos_bonus = questoes_corretas ** 2
+print("\n--- VERIFICAÇÃO DE ACESSO ---")
 
-print("\n--- RESUMO DE DESEMPENHO ---")
-print(f"Soma das notas: {soma_notas:.2f}")
-print(f"Média: {media:.2f}")
-print(f"Diferença entre as notas: {diferenca:.2f}")
-print(f"Tempo de estudo: {horas_estudo} h e {minutos_restantes} min")
-print(f"Aproveitamento: {aproveitamento:.1f}%")
-print(f"Pontos de bônus: {pontos_bonus}")
+if idade >= idade_minima:
+   print(f"{nome}, seu acesso à oficina foi liberado.")
+   print("Você já possui a idade mínima exigida.")
+else:
+   anos_faltantes = idade_minima - idade
+   print(f"{nome}, seu acesso ainda não foi liberado.")
+   print(f"Faltam {anos_faltantes} ano(s) para atingir a idade mínima.")
